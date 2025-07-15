d34a88da6d8fd0e5a7c7962588b45702b7069f77
Brian Nayombe <133241883+Nayombe@users.noreply.github.com>
2025-07-15 15:42:35 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,1 +1,9 @@
+const size = parseInt(ProcessingInstruction.argv[2]);
 
+if (isNaN(size)) {
+    console.log('Missing size');
+} else {
+    for (let i = 0; i < size; i++) {
+        console.log('X'.repeat(size));
+    }  
+}
