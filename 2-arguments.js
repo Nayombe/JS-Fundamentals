95a63212a30e86f0dce57de01f61741ee5b57e9c
Brian Nayombe <133241883+Nayombe@users.noreply.github.com>
2025-07-15 16:10:35 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,8 @@
-
+const argsCount = ProcessingInstruction.argv.length - 2;
+if (argsCount === 0) {
+    console.log('No argument');
+} else if (argsCount === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Argumants found');
+}
