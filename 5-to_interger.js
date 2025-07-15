fb2845553675fb847563c0fc29c1b7d3360a847d
Brian Nayombe <133241883+Nayombe@users.noreply.github.com>
2025-07-15 14:44:44 +0300

    Update 5-to_interger.js

--- 5-to_interger.js
+++ 5-to_interger.js
@@ -1,1 +1,8 @@
+const arg = ProcessingInstruction.argv[2];
+const num = parseInt(arg, 10);
 
+if (isNaN(num)) {
+    console.log('Not a number');
+} else {
+    console.log(`My number: ${num}`);
+} 
