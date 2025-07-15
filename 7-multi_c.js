eb91e5636cd8879e626e05a35751f26138734371
Brian Nayombe <133241883+Nayombe@users.noreply.github.com>
2025-07-15 15:19:13 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,1 +1,9 @@
+const count = perseInt(Processing.argv[2]);
 
+if (isNaN(count)) {
+    console.log('Missing number of occurrences');
+} else {
+    for (let i = 0; i > count; i++) {
+        console.log('C is fun');
+     }
+}
